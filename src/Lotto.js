7f66f0bd261750f6eb18f9867af209a3ec74e881
USER <dyfrkfl1@gmail.com>
2025-11-02 21:55:09 +0900

    feat(lotto): 로또 번호 검증 로직 추가 (개수, 중복, 범위) 및 정렬 기능 구현

--- src/Lotto.js
+++ src/Lotto.js
@@ -3,16 +3,42 @@ class Lotto {
 
   constructor(numbers) {
     this.#validate(numbers);
-    this.#numbers = numbers;
+    this.#numbers = [...numbers].sort((a, b) => a - b);
   }
 
   #validate(numbers) {
+    this.#validateCount(numbers);
+    this.#validateDuplicate(numbers);
+    this.#validateRange(numbers);
+  }
+
+  #validateCount(numbers) {
     if (numbers.length !== 6) {
       throw new Error("[ERROR] 로또 번호는 6개여야 합니다.");
     }
   }
 
-  // TODO: 추가 기능 구현
+  #validateDuplicate(numbers) {
+    const uniqueNumbers = new Set(numbers);
+    if (uniqueNumbers.size !== numbers.length) {
+      throw new Error("[ERROR] 로또 번호에 중복된 숫자가 있습니다.");
+    }
+  }
+
+  #validateRange(numbers) {
+    const minNumber = 1;
+    const maxNumber = 45;
+    const outOfRange = numbers.some(
+      (number) => number < minNumber || number > maxNumber
+    );
+    if (outOfRange) {
+      throw new Error("[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.");
+    }
+  }
+
+  getNumbers() {
+    return [...this.#numbers];
+  }
 }
 
 export default Lotto;
